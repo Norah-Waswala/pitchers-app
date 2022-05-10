e1b4d7ea0ed573d53eaa37e4227d9e7940ebabca
Norah.Waswala <norah.waswala@student.moringa.com>
2022-05-10 12:16:23 +0300

    refactor: change development mode to production

--- manage.py
+++ manage.py
@@ -3,8 +3,8 @@ from flask_script import Manager,Server
 from app.models import User,Pitch
 from flask_migrate import Migrate,MigrateCommand
 # Creating app instance
-app = create_app('production')
 
+app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 migrate=Migrate(app,db)
