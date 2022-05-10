c897fe574e071853bb2d40ce0801f107069944d9
Norah.Waswala <norah.waswala@student.moringa.com>
2022-05-10 17:35:01 +0300

    styles:add margin and color

--- app/main/views.py
+++ app/main/views.py
@@ -68,7 +68,7 @@ def updateprofile(name):
         abort(404)
     if form.validate_on_submit():
         user.bio = form.bio.data
-        user.save()
+        user.save_u()
         return redirect(url_for('.profile',name = name))
     return render_template('profile/update.html',form =form)
 
