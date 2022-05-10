278eb22d726cc157929a04fdc4de39a7157e1c8a
Norah.Waswala <norah.waswala@student.moringa.com>
2022-05-10 17:42:00 +0300

    refactor:change development to production mode

--- manage.py
+++ manage.py
@@ -4,14 +4,20 @@ from app.models import User,Pitch
 from flask_migrate import Migrate,MigrateCommand
 # Creating app instance
 
-app = create_app('development')
+app = create_app('production')
 manager = Manager(app)
-manager.add_command('server',Server)
+manager.add_command('run',Server(use_debugger=True))
 migrate=Migrate(app,db)
 manager.add_command('db',MigrateCommand)
+
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User,Pitch = Pitch)
+    return dict(app = app,db = db,User = User,Pitch = Pitch,User=User)
 
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 if __name__ == '__main__':
     manager.run()
