8d215a0fa3e2afa2db8f836abf19b1055062a419
Norah.Waswala <norah.waswala@student.moringa.com>
2022-05-16 15:13:11 +0300

    deployment

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 import os
-# from dotenv import load_dotenv
-# load_dotenv()
+
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://norah:12345we@localhost/pitches'
+    
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     SECRET_KEY=os.environ.get('SECRET_KEY')
-    # UPLOADED_PHOTOS_DEST ='app/static/photos'
+   
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
@@ -20,6 +19,7 @@ class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = "postgresql://hyenxhrfgrqwyg:2c18d6ec2b484eaf209df68c6869fba74a02d054c1ac171d945c087285b966e3@ec2-44-195-169-163.compute-1.amazonaws.com:5433/dafsk8fdljk1nl"
        
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://norah:12345we@localhost/pitches'
     DEBUG = True
 
 config_options = {
